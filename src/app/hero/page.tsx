b6ae8b7e1a8ba70f135a823ea63e1c451283d79d
Collyngodlewski <collyngodlewski@gmail.com>
2023-05-21 18:48:17 -0400

    error while building

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -1,33 +1,15 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const banner = {
-  animate: {
-    transition: {
-      delayChildren: 0.4,
-      staggerChildren: 0.1,
-    },
-  },
-};
-
 function Hero() {
   return (
-    <div>
-      {" "}
-      <div className="home-page">
-        <motion.div className="animated-container" variants={banner}>
-          <div className="name">Collyn</div>
-          <motion.div
-            initial={{ x: 1500 }}
-            animate={{ x: -1500 }}
-            transition={{ duration: 15, repeat: Infinity }}
-            className="scroll-effect"
-          >
-            Frontend Web Developer
-          </motion.div>
-          <div className="name">Godlewski</div>
-        </motion.div>
+    <div className="home-page">
+      <div className="animated-container">
+        <div className="name">Collyn</div>
+        Frontend Web Developer
       </div>
+      <div className="name">Godlewski</div>
+      <div></div>
     </div>
   );
 }
